bde27190fe0584abcef7605db5ba890794ec33f5
PersesTitan <torampersestitan@gmail.com>
2022-04-05 13:11:23 +0900

    commit

--- src/main/java/Variable/VariableSet.java
+++ src/main/java/Variable/VariableSet.java
@@ -3,8 +3,8 @@ package Variable;
 import Setting.Local;
 import Setting.Setting;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-import javax.validation.constraints.NotNull;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
